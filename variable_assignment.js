4fc8ed723d648119c203d32e33bd165eafdbba4c
akashmungase <106262085+akashmungase@users.noreply.github.com>
2022-06-19 18:35:41 +0530

    Add files via upload

---
+++ variable_assignment.js
@@ -0,0 +1,38 @@
+//this is my assignment
+console.log("Assignment 1");
+var a="Akash";
+console.log("first name: ",a);
+var b="mungase";
+console.log("last name: ",b);
+var c="akashmungase99@gmail.com";
+console.log("Email: ",c);
+var d="experienced";
+console.log("work status: ",d);
+var e="22";
+console.log("Age: ",e);
+var f="Male";
+console.log("Gender: ",f);
+
+
+console.log("Assignment 2");
+console.log("Before changing value");
+var x=10; 
+console.log(x);
+console.log("After changing value ");
+x=20;
+console.log(x);
+
+
+console.log("Assignment 3");
+var g="100";
+var h="200";
+var i="";
+console.log(g);
+console.log(h);
+console.log("After swipe ");
+i=g;
+g=h;
+console.log(g);
+console.log(i);
+
+ 
