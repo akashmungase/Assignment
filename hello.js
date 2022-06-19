ce340900272a14f6721601dac909a258cf1fd1af
akashmungase <106262085+akashmungase@users.noreply.github.com>
2022-06-19 18:29:33 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,35 @@
+// this is my first hello world program in js
+/*
+this is multi-line comments 
+*/
+console.log( "hello js, I am akash");
+console.log("how are you?");
+console.log("Tell me about yourself?");
+console.log("Hello sir ");
+console.log("Good evening ");
+console.log("My name akash mungase. I am engineering Graduate");
+console.log(" I have 1 year hands on experience in development and technological tacks used HTML5, CSS, Bootstrap, JavaScript and angular ");
+console.log(" I am using tools like git and github ");
+console.log(" Currently I am working in sevenmind solution organization ");
+console.log(" I got this opportunity to work in E-commerce domain for the project Hotel management system");
+console.log(" Hotel management system is a dynamic website ");
+console.log("to provide costumer with flexibility in checking ");
+console.log(" costumer can easily see availability of rooms and they can book online ");
+console.log(" costumer can see food and their prices and they can order online it ");
+console.log(" as hotel manager keeps updating the hotel management staff quickly, such as changing prices, updating available rooms ");
+console.log(" thus, with a hotel management system, online room booking turned out more comfortable ");
+console.log(" in this project we are used technological tacks HTML5, CSS, Bootstrap, JavaScript and Angular  ");
+console.log(" in this project my role is project developer, I was responsible for create new pages and develop the project ");
+console.log(" I developed sign in and sign up form by using HTML5, CSS, Bootstrap ");
+console.log(" Coming my academic details i have completed B.Tech in civil engineering sangola with 77% in 2021 ");
+console.log(" I have completed my HSC in Damaji college Mangalwedha with 63% in 2017 ");
+console.log(" I have completed my SSC in RSVT Highschool Talasngi with 81% in 2015 ");
+console.log(" my hobbies  ");
+console.log(" Playing kabddi and kho-kho ");
+console.log(" Technological tacks ");
+console.log("HTML5, CSS, Bootstrap, JavaScript, Angular")
+console.log(" *************************************************************************** ");
+console.log(" Company name: Sevenmid Technology ");
+console.log(" ##########################################################################");
+console.log(" Today I have lear - Nodejs installation and hello word program ");
+console.log(" ************************************************************************** ")
