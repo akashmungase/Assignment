5ed4d5c9b87038e67054f4f847ce084b58c3388d
akashmungase <106262085+akashmungase@users.noreply.github.com>
2022-06-22 12:42:52 +0530

    Add files via upload

---
+++ string_templete_assignment.js
@@ -0,0 +1,20 @@
+var mydreamcompany = "Infosys";
+console.log(`1. My dream company is: ${mydreamcompany}`);
+
+var myHobbies1 = "Reading books";
+var myHobbies2 = "Playing Kabddi";
+var myHobbies3 = "Playing Kho-Kho"; 
+
+console.log("2.1 My Hobbies are: ");
+console.log(myHobbies1);
+console.log(myHobbies2);
+console.log(myHobbies3);
+console.log(`2.2. My Hobbies are: ${myHobbies1}, ${myHobbies2}, ${myHobbies3}`);
+
+
+var number1 = 500; var number2 = 700;
+console.log("Before swaping: ", number1, number2);
+number1 = number1 + number2;
+number2 = number1 - number2;
+number1 = number1 - number2;
+console.log(`Aefore swaping:  ${number1} ${number2} `);
