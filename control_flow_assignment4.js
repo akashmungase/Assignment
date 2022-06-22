2de09c8703dfedf98fac8dba2c7ffa5c7bd988ee
akashmungase <106262085+akashmungase@users.noreply.github.com>
2022-06-22 12:41:32 +0530

    Add files via upload

---
+++ control_flow_assignment4.js
@@ -0,0 +1,57 @@
+function monthOFYear(n){
+switch (n) {
+    case n=1:
+        console.log("January");
+        break;
+    case n=2:
+        console.log("February");
+        break;
+    case n=3:
+        console.log("March");
+        break;
+    case n=4:
+        console.log("April");
+        break;
+    case n=5:
+        console.log("May");
+        break;
+    case n=6:
+        console.log("June");
+        break;
+    case n=7:
+        console.log("July");
+        break;
+    case n=8:
+        console.log("August");
+        break;
+    case n=9:
+        console.log("September");
+        break;
+    case n=10:
+        console.log("October");
+        break;
+    case n=11:
+        console.log("November");
+        break;
+    case n=12:
+        console.log("December");
+        break;
+    default:
+        console.warn("Invalid Input");
+}
+}
+monthOFYear(1);
+monthOFYear(2);
+monthOFYear(3);
+monthOFYear(4);
+monthOFYear(5);
+monthOFYear(6);
+monthOFYear(7);
+monthOFYear(8);
+monthOFYear(9);
+monthOFYear(10);
+monthOFYear(11);
+monthOFYear(12);
+monthOFYear(13);
+console.log("============================================")
+console.log("Special Character, String, Different Number");
